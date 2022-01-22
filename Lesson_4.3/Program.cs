71f6d950805654f4f0bbcad67327ebf6c410f656
Eugene <comradeeugene@gmail.com>
2022-01-22 22:44:59 +0300

    first

--- Lesson_4.3/Program.cs
+++ Lesson_4.3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Lesson_4
 {
@@ -6,7 +7,36 @@ namespace Lesson_4
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random RandomNum = new Random();
+
+            WriteLine("Введите максимальное значение диапазона");
+            int Max = int.Parse(ReadLine());
+
+            int RandomValue = RandomNum.Next(0, Max + 1);
+
+            while (true)
+            {
+                WriteLine("Угадайте часло или нажмите Enter для выхода");
+                string Pick = ReadLine();
+                if (Pick == "")
+                {
+                    return;
+                }
+                else if (int.Parse(Pick) < RandomValue)
+                {
+                    WriteLine("Это число меньше угадываемого");
+                }
+                else if (int.Parse(Pick) > RandomValue)
+                {
+                    WriteLine("Это число больше угадываемого");
+                }
+                else
+                {
+                    WriteLine($"{RandomValue} - правильное число");
+                    break;
+                }
+            }
+            ReadKey();
         }
     }
 }
